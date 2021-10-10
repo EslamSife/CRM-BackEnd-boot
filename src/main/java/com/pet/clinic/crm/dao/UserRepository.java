d6406e6044249bb23cdd4f16b5949da7ed0a864f
EslamSife <eslamsife91@gmail.com>
2021-10-10 13:33:55 +0200

    remove controller and service for user

--- src/main/java/com/pet/clinic/crm/dao/UserRepository.java
+++ src/main/java/com/pet/clinic/crm/dao/UserRepository.java
@@ -2,8 +2,8 @@ package com.pet.clinic.crm.dao;
 
 import com.pet.clinic.crm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 public interface UserRepository extends JpaRepository<User, Integer> {
-
-
 }
